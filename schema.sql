67f1171af6ea8d061acb4eb054112ca871e0cbb4
Brendan Berry <53982094+Pickingberries@users.noreply.github.com>
2019-12-17 19:33:02 -0800

    created schema, package.json, and added template for the server file

---
+++ schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department(
+id INT AUTO_INCREMENT NOT NULL,
+name VARCHAR(30),
+PRIMARY KEY (id),
+);
+
+CREATE TABLE role(
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT REFERENCES department(id),
+);
+
+CREATE TABLE employee(
+id INT AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30)
+last_name VARCHAR(30)
+role_id INT REFERENCES role(id)
+manager_id INT REFERENCES role(title)
+);
